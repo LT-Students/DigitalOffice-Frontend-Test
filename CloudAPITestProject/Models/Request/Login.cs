4d0231d2a46032175afc527edfa0edcad86199c0
Egorka <belehov.egor2001@yandex.ru>
2021-04-26 20:43:46 +0300

    initial

---
+++ CloudAPITestProject/Models/Request/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudAPITestProject.Models.Request
+{
+    class Login
+    {
+        public string LoginData { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ CloudAPITestProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using CloudAPITestProject.Models.Request;
+using System.Net.Http;
+
+namespace CloudAPITestProject
+{
+    class Program
+    {
+        private static ServiceUrls _servicesInfo;
+
+        static void Main(string[] args)
+        {
+            var request = new Request(
+                "CheckService_Login",
+                "Login with wrong login",
+                "https://auth.ltdo.xyz/auth/login",
+                HttpMethod.Post);
+
+            var login = new Login { LoginData = "login", Password = "Password" };
+
+            request.Body = login;
+                //"{\n\"LoginData\": \"login\",\n\"Password\": \"password\"\n}";
+
+            var result = request.SendRequest();
+        }
+    }
+}
