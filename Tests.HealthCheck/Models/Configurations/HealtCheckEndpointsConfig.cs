fcd6251c6df18da2ffbaeaf6980ba253693de0f4
SolovyovDmitry <101804724+SolovyovDmitry@users.noreply.github.com>
2022-09-12 14:49:21 +0300

    remove services except department (#20)

--- Tests.HealthCheck/Models/Configurations/HealtCheckEndpointsConfig.cs
+++ Tests.HealthCheck/Models/Configurations/HealtCheckEndpointsConfig.cs
@@ -4,25 +4,25 @@ namespace Tests.HealthCheck.Models.Configurations
     {
         public const string SectionName = "HealthCheckEndpoints";
 
-        public string UserHealthCheckEndpoint { get; set; }
-        public string ProjectHealthCheckEndpoint { get; set; }
-        public string TimeHealthCheckEndpoint { get; set; }
-        public string NewsHealthCheckEndpoint { get; set; }
-        public string MessageHealthCheckEndpoint { get; set; }
-        public string RightsHealthCheckEndpoint { get; set; }
-        public string FileHealthCheckEndpoint { get; set; }
-        public string CompanyHealthCheckEndpoint { get; set; }
-        public string AuthHealthCheckEndpoint { get; set; }
-        public string SearchHealthCheckEndpoint { get; set; }
-        public string HistoryHealthCheckEndpoint { get; set; }
-        public string TaskHealthCheckEndpoint { get; set; }
-        public string EmailHealthCheckEndpoint { get; set; }
-        public string PositionHealthCheckEndpoint { get; set; }
-        public string DepartmentHealthCheckEndpoint { get; set; }
-        public string OfficeHealthCheckEndpoint { get; set; }
-        public string EducationHealthCheckEndpoint { get; set; }
-        public string AdminHealthCheckEndpoint { get; set; }
-        public string ImageHealthCheckEndpoint { get; set; }
+    //public string UserHealthCheckEndpoint { get; set; }
+    //public string ProjectHealthCheckEndpoint { get; set; }
+    //public string TimeHealthCheckEndpoint { get; set; }
+    //public string NewsHealthCheckEndpoint { get; set; }
+    //public string MessageHealthCheckEndpoint { get; set; }
+    //public string RightsHealthCheckEndpoint { get; set; }
+    //public string FileHealthCheckEndpoint { get; set; }
+    //public string CompanyHealthCheckEndpoint { get; set; }
+    //public string AuthHealthCheckEndpoint { get; set; }
+    //public string SearchHealthCheckEndpoint { get; set; }
+    //public string HistoryHealthCheckEndpoint { get; set; }
+    //public string TaskHealthCheckEndpoint { get; set; }
+    //public string EmailHealthCheckEndpoint { get; set; }
+    //public string PositionHealthCheckEndpoint { get; set; }
+    public string DepartmentHealthCheckEndpoint { get; set; }
+    //public string OfficeHealthCheckEndpoint { get; set; }
+    //public string EducationHealthCheckEndpoint { get; set; }
+    //public string AdminHealthCheckEndpoint { get; set; }
+    //public string ImageHealthCheckEndpoint { get; set; }
 
-    }
+  }
 }
